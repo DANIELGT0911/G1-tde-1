6b33cd100d898cabb6c25e48f965a43f1c1f47db
DANIELGT0911 <166229603+DANIELGT0911@users.noreply.github.com>
2025-06-02 20:37:45 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package main;
+
+import controller.AutomovelController;
+import view.AutomovelView;
+
+public class Main {
+    public static void main(String[] args) {
+        AutomovelController controller = new AutomovelController();
+        AutomovelView view = new AutomovelView(controller);
+        view.exibirMenu();
+    }
+}
